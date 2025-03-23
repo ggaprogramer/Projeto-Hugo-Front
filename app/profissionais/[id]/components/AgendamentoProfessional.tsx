5dbe41bbc87c03f2c91853bdc0f0601597a86367
ggaprogramer <guiorganization@gmail.com>
2025-03-23 17:33:19 -0300

    Ajeitando o calendário e responsividade da página do profissional

--- app/profissionais/[id]/components/AgendamentoProfessional.tsx
+++ app/profissionais/[id]/components/AgendamentoProfessional.tsx
@@ -1,34 +1,95 @@
 "use client";
 
 import '../styles/agendamentos-professional.scss';
-import { Calendar } from 'primereact/calendar';
 import {useRef, useState, useEffect} from 'react';
 
-import 'primereact/resources/themes/saga-blue/theme.css';
-import 'primereact/resources/primereact.min.css';
-import 'primeicons/primeicons.css';
-import { locale, addLocale } from 'primereact/api';
-import { Nullable } from "primereact/ts-helpers";
+import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css';
 
-addLocale('pt-BR', {
-    firstDayOfWeek: 0,
-    dayNames: ['domingo', 'segunda-feira', 'terça-feira', 'quarta-feira', 'quinta-feira', 'sexta-feira', 'sábado'],
-    dayNamesShort: ['dom', 'seg', 'ter', 'qua', 'qui', 'sex', 'sáb'],
-    dayNamesMin: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'],
-    monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
-    monthNamesShort: ['jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez'],
-    today: 'Hoje',
-    clear: 'Limpar',
-});
+interface Dates {
+    day: Date,
+    hours: string[]
+}
+
+const dates: Dates[] = [
+    { day: new Date(2025, 2, 15), hours: [
+        '08:00',
+        '09:00',
+        '10:00',
+        '11:00',
+    ]},
+    { day: new Date(2025, 2, 16), hours: [
+        '08:00',
+        '09:00',
+        '10:00',
+        '11:00',
+    ]},
+    { day: new Date(2025, 2, 17), hours: [
+        '08:00',
+        '09:00',
+        '10:00',
+        '11:00',
+    ]},
+    { day: new Date(2025, 2, 20), hours: [
+        '12:00',
+        '13:00',
+        '14:00',
+        '15:00',
+    ]},
+    { day: new Date(2025, 2, 22), hours: [
+        '16:00',
+        '17:00',
+        '18:00',
+        '19:00',
+    ]}
+];
 
 export default function AgendamentoProfessional(){
-    const [date, setDate] = useState<Nullable<Date>>(null);
+    const dateNow = new Date();
+    const [dateSelected, setDateSelected] = useState(dateNow);
+    const [hourSelected, setHourSelected] = useState<string | null>();
+    const [hoursOfDate, setHoursOfDate] = useState<string[]>([]);
 
     useEffect(() => {
-        locale('pt-BR');
-    }, []);
+        for(let i = 0; i < dates.length; i++){
+            if(dates[i].day.toDateString() === dateSelected.toDateString()){
+                setHoursOfDate([...dates[i].hours]);
+                break;
+            }
+        }
+        setHourSelected(null);
+    }, [dateSelected]);
 
+    // Função para verificar se um dia tem agendamento
+    const haveAgendamento = (dateCalendar: any) => {
+        return dates.some((date) => date.day.toDateString() === dateCalendar.toDateString());
+    };
 
+    const variableHaveAgendamento = haveAgendamento(dateSelected);
+
+    // Classe CSS para os dias com agendamentos disponíveis
+    const tileClassName = ({ date, view }: any) => {
+        if (view === 'month' && haveAgendamento(date)) {
+        return 'selected'; // Adiciona uma classe CSS para dias com agendamento disponível
+        }
+        return null;
+    };
+
+    const onChange = (novaData: any) => {
+        setDateSelected(novaData);
+    };
+
+    const formatDescriptionOne = () => {
+        if(dateSelected.toDateString() === dateNow.toDateString()){
+            return 'hoje';
+        } else if(dateSelected.getDate() === dateNow.getDate() + 1 &&
+        dateSelected.getMonth() === dateNow.getMonth() && 
+        dateSelected.getFullYear() === dateNow.getFullYear()) {
+            return 'amanhã';
+        } else{
+            return dateSelected.toLocaleDateString();
+        }
+    }
 
     return (
         <div className='info-agendamentos'>
@@ -38,9 +99,35 @@ export default function AgendamentoProfessional(){
             <p>
                 Selecione o dia e horário de sua preferência para agendar sua consulta com a Dra. Sofia Mendes.
             </p>
-            <div className="card flex justify-content-center" style={{border: '1px solid black', width: '100%'}}>
-                <Calendar dateFormat="dd/mm/yy" locale='pt-BR' value={date} onChange={(e) => setDate(e.value)} inline className="custom-calendar" />
-            </div>
+            <Calendar onChange={onChange}
+                value={dateSelected}
+                tileClassName={tileClassName}
+                className='custom-calendar'
+            />
+            {variableHaveAgendamento && 
+            <p>
+                Horários disponíveis para {formatDescriptionOne()}
+            </p>}
+            {variableHaveAgendamento &&
+                <div className='horarios'>
+                    {hoursOfDate.map(horario => {
+                        if(horario === hourSelected){
+                            return (<span key={horario} onClick={() => setHourSelected(horario)} className='horario selecionado'>
+                                {horario}
+                            </span>)
+                        } else {
+                            return (<span key={horario} onClick={() => setHourSelected(horario)} className='horario'>
+                                {horario}
+                            </span>)
+                        }
+                    })}
+                </div>
+            }
+            
+            {dateSelected && 
+            hourSelected && 
+            <button>Confirmar agendamento para {dateSelected.toLocaleDateString()} às {hourSelected}</button> }
+
         </div>
     )
 }
